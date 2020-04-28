6bc0c02d9c7d2caca566664b893869619fb17a6b
Camilo Moncada <monk-da@hotmail.com>
2020-04-28 15:26:26 -0500

    response question 6 print comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for n in range(1, 10):
+        if (n + i) < 10:
+            if i != 8:
+                print("{0:0d}{1:0d}, ".format(i, (n + i)), end="")
+            else:
+                print("{0:0d}{1:0d}".format(i, (n + i)), end="")
