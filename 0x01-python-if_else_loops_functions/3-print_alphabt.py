b3d8cf37de01e9296d4a4ebdf462d122265248b4
Camilo Moncada <monk-da@hotmail.com>
2020-04-28 12:01:38 -0500

    corections question 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 for i in range(97,123):
-    if (i == 101) or (i == 113):
-        continue
-    print(chr(i), end="")
+    if i not in [101,113]:
+        print(chr(i), end="")
