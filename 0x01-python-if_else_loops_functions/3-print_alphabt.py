9e8ba25d94bd4e3396465d12b4cae15b384c05b1
Camilo Moncada <monk-da@hotmail.com>
2020-04-28 12:21:09 -0500

    bugfix question 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97,123):
-    if i not in [101,113]:
-        print(chr(i), end="")
+for i in range(0, 26):
+    if i not in [4,16]:
+        print("{:s}".format(chr(ord('a') + i)), end="")
