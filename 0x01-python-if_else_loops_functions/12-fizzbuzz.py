d19da1ddfadc2511160bafe1147dc118eba32942
Camilo Moncada <monk-da@hotmail.com>
2020-04-28 19:35:33 -0500

    bugfix 2 question 12 fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 def fizzbuzz():
+    a = "Fizz"
+    b = "Buzz"
     for i in range(1, 101):
         if(i % 3 == 0 and i % 5 == 0):
-            print("FizzBuzz ", end="")
+            print("{:s}{:s} ".format(a,b), end="")
         elif(i % 3 == 0):
-            print("Fizz ", end="")
+            print("{:s} ".format(a), end="")
         elif(i % 5 == 0):
-            print("Buzz ", end="")
+            print("{:s} ".format(b), end="")
         else:
             print("{:d} ".format(i), end="")
