9217b0c2412780b8f6f5621f88d15cb37e037bd4
Camilo Moncada <monk-da@hotmail.com>
2020-04-30 13:03:15 -0500

    response question 0 add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import add_0
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+<h1>Learn about import modules in python</h1>
