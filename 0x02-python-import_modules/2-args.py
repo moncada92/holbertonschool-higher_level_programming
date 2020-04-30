668f0b37cd8f2c190df2033ef0b64325ca70d213
Camilo Moncada <monk-da@hotmail.com>
2020-04-30 17:58:10 -0500

    response question 2 args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    count = len(args)
+    itr = 1
+    if count > 0:
+        print("{:d} arguments:".format(count))
+        for x in args:
+            print("{:d}: {:s}".format(itr, x))
+            itr += 1
+    else:
+        print("{:d} arguments.".format(count))
