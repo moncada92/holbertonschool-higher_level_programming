51f60610e98c83a3fb0e2063b6f48da8458fec49
Camilo Moncada <monk-da@hotmail.com>
2020-04-30 22:48:52 -0500

    response advanced question 100 calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    args = sys.argv[1:]
+    count = len(args)
+
+    if count == 3:
+
+        a = int(args[0])
+        b = int(args[2])
+
+        if args[1] == '+':
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+        elif args[1] == '-':
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+
+        elif args[1] == '*':
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+
+        elif args[1] == '/':
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
