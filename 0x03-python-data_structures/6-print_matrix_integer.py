f53f2cd18eb275d4b15eac2dbd5cd366c2301f87
Camilo Moncada <monk-da@hotmail.com>
2020-05-05 17:50:35 -0500

    bugfix 2 question 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,11 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None or matrix == [[]]:
         print()
     for row in matrix:
+        k = 1
         for element in row:
-            print("{:d}".format(element), end=' ')
-        print()
+            if k < len(row):
+                print("{:d} ".format(element), end='')
+            else:
+                print("{:d}".format(element))
+
+            k += 1
