4595cfc4da01205fdb9ce0fc928e8457722d583a
Camilo Moncada <monk-da@hotmail.com>
2020-05-05 17:43:49 -0500

    bugfix question 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None or matrix == [[]]:
+        print()
     for row in matrix:
         for element in row:
             print("{:d}".format(element), end=' ')
