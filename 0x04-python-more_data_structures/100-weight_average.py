20f4249416be721828200c92cac4e5cebd8f0d4d
Camilo Moncada <monk-da@hotmail.com>
2020-05-07 23:30:23 -0500

    response question 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    num = 0
+    den = 0
+    result = 0.0
+    for i, j in my_list:
+        num = num + (i * j)
+        den = den + j
+
+    result = num / den
+    return result
