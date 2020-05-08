61ae58a275f33378985256fc8c2858698a0cd5a8
Camilo Moncada <monk-da@hotmail.com>
2020-05-07 22:35:21 -0500

    response question 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    score = None
+    student = None
+
+    if a_dictionary != None:
+        for k, v in a_dictionary.items():
+            if score is None or score < v:
+                score = v
+                student = k
+
+    return student
