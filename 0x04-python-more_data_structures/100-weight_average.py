e8e99cbace50169dc3aa25cf265eeb69440548f4
Camilo Moncada <monk-da@hotmail.com>
2020-05-07 23:32:55 -0500

    bugfix question 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,9 +3,10 @@ def weight_average(my_list=[]):
     num = 0
     den = 0
     result = 0.0
-    for i, j in my_list:
-        num = num + (i * j)
-        den = den + j
+    if len(my_list) != 0:
+        for i, j in my_list:
+            num = num + (i * j)
+            den = den + j
 
     result = num / den
     return result
