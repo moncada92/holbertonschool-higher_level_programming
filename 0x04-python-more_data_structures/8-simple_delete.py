007182d4e3457c7a3e6d8390906bf12163f22652
Camilo Moncada <monk-da@hotmail.com>
2020-05-07 21:35:59 -0500

    response question 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def simple_delete(a_dictionary, key=""):
+    iskey = False
+    for llave in a_dictionary:
+        if llave == key:
+            iskey = True
+
+    if iskey:
+        del a_dictionary[key]
+
+    return a_dictionary
