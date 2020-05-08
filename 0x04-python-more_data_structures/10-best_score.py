568406814f03964f465cf7246b42c14140c4216d
Camilo Moncada <monk-da@hotmail.com>
2020-05-07 22:36:57 -0500

    bugfix question 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     score = None
     student = None
 
-    if a_dictionary != None:
+    if a_dictionary is not None:
         for k, v in a_dictionary.items():
             if score is None or score < v:
                 score = v
