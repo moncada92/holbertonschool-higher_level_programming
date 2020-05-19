c50376876e9535f2e3b52ca578a914a490ccd7fc
Camilo Moncada <monk-da@hotmail.com>
2020-05-19 17:35:54 -0500

    hotfix 3 question 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
     """Write an empty class Square that defines a square:"""
-    pass
+    def __init__(self):
+        """Initialization of the class square."""
+        pass
