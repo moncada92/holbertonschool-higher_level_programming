5fb138771a2deb092a5e4eb1a2734965c04fcbb5
Camilo Moncada <monk-da@hotmail.com>
2020-05-23 17:39:02 -0500

    hotfi 3 question 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python3
 """
-Function divider a itmes the matrix
-Return: matrix / for 3
-
+Module matrix_divided
+Divided matrix
 """
 
 
 def matrix_divided(matrix, div):
+    """Returns a matrix
+    of results of a divided matrix
+    """
 
-    """Function that divide a matrix"""
-    divide = []
-    errsize = "Each row of the matrix matrix must have the same size"
-    errtype = "matrix must be a matrix (list of list) of integers/floats"
-    rowback = len(matrix[0])
+    e = "matrix must be a matrix (list of lists) of integers/floats"
 
-    if type(div) not in [int, float]:
+    if type(matrix) != list or len(matrix) == 0 or matrix[0] is None:
+        raise TypeError(e)
+
+    for r in matrix:
+        if len(r) == 0:
+            raise TypeError(e)
+        for i in r:
+            if type(i) != int and type(i) != float:
+                raise TypeError(e)
+
+    lr = []
+    for r in matrix:
+        lr.append(len(r))
+    if not all(item == lr[0] for item in lr):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    for i in range(len(matrix)):
-        divide.append([])
-        if(len(matrix[i]) is not rowback):
-            raise TypeError(errsize)
-        for j in range(len(matrix[i])):
-            if type(matrix[i][j]) not in [int, float]:
-                raise TypeError(errtype)
-            divide[i].append(round(matrix[i][j] / div, 2))
-            rowback = len(matrix[i])
-    return divide
+
+    nm = [[round(x / div, 2) for x in r] for r in matrix]
+
+    return nm
