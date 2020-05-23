051da725fafca490c119054111d7d00e526058b1
Camilo Moncada <monk-da@hotmail.com>
2020-05-23 17:46:53 -0500

    hotfix 3 question 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,16 +7,16 @@ Return: squares
 
 
 def print_square(size):
-    """print square"""
-    if type(size) is not int:
-        raise("size must be an integer")
-
+    """
+    Print a square
+    """
+    if type(size) != int:
+        raise TypeError('size must be an integer')
     if size < 0:
-        raise ValueError("size must be >= 0")
-
-    for i in range(0, size):
-        j = 0
-        while (size > j):
+        raise ValueError('size must be >= 0')
+    if size == 0:
+        return
+    for i in range(size):
+        for j in range(size):
             print("#", end="")
-            j += 1
-        print("")
+        print()
