f3a92483c0e3c0fb409349e63e84c3f8efd003b4
Camilo Moncada <monk-da@hotmail.com>
2020-05-23 17:22:10 -0500

    hotfix question 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,15 +2,11 @@
 """
 Function add two integers
 Return: a+b in cast integer
-
 """
 
 
 def add_integer(a, b=98):
-    """
-    Function that validate the add of two integers
-    """
-
+    """Function that validate the add of two integers"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
