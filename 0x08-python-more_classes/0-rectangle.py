29a12accecc6fa3864bfa61a0fdaafdefdbd4db0
Camilo Moncada <monk-da@hotmail.com>
2020-05-25 16:30:44 -0500

    response question 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this module  defines the Rectangle class"""
+
+
+class Rectangle:
+    """This is a class of a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+<h1>Learn about More Classes and Objects in Python</h1>
