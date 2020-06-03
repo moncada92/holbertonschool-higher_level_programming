e6437dbfc11c746b67f538ee9f3f4a6085d22b48
Camilo Moncada <monk-da@hotmail.com>
2020-06-02 23:23:02 -0500

    hotfix 2 question advanced 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,9 +8,8 @@ module for MyInt
 class MyInt(int):
     """This is a class of a MyInt"""
 
-    def __ne__(self, value):
-        return super().__eq__(value)
+    def __ne__(self, other):
+        return super().__eq__(other)
 
-    def __eq__(self, value):
-        return super().__ne__(value)
-    
+    def __eq__(self, other):
+        return super().__ne__(other)
