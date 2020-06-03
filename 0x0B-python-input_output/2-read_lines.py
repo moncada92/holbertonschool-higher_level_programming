be173bbc4f4f9a5c8ba9d7c54119ae248745fe6a
Camilo Moncada <monk-da@hotmail.com>
2020-06-03 17:22:15 -0500

    hotifx question 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -9,13 +9,13 @@ def read_lines(filename="", nb_lines=0):
     """number lines text file"""
     num_lines = 0
 
-    with open("my_file_0.txt", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         for line in f:
             num_lines += 1
 
     if nb_lines <= 0 or nb_lines > num_lines:
         nb_lines = num_lines
 
-    with open("my_file_0.txt", encoding="utf-8") as f2:
+    with open(filename, encoding="utf-8") as f2:
         for l in range(nb_lines):
             print(f2.readline(), end="")
