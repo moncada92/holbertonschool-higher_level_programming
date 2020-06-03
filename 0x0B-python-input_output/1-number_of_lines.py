e59447fa6b0656cdc2300731a1baf4fc8b8558c4
Camilo Moncada <monk-da@hotmail.com>
2020-06-03 17:12:28 -0500

    hotfix questions

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -8,7 +8,7 @@ module for number_lines.
 def number_of_lines(filename=""):
     """number lines text file"""
     num_lines = 0
-    with open("my_file_0.txt") as f:
+    with open("my_file_0.txt", encoding="utf-8") as f:
         for line in f:
             num_lines += 1
 
