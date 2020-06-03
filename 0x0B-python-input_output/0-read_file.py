00e5e4a6d317f0177b6944609032b545c462f2c1
Camilo Moncada <monk-da@hotmail.com>
2020-06-03 17:16:08 -0500

    hotfix 2 question 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,6 @@ module for read_file.
 
 def read_file(filename=""):
     """Read a text file"""
-    with open("my_file_0.txt", 'r',encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+    with open("my_file_0.txt", 'r', encoding="utf-8") as f:
+        content = read_data = f.read()
+    print(content, end="")
