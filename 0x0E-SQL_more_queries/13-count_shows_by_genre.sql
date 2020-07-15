e5dc6ef12aa0aa4f41329455026dddee26bf4d29
Camilo Moncada <monk-da@hotmail.com>
2020-07-15 14:08:35 -0500

    response question 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows
+-- user_0d_2 on your server (in localhost).
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres 
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
