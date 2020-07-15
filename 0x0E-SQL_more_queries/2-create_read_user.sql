0bfb15ded424c3ea84662601ea2d82a411cfe099
Camilo Moncada <monk-da@hotmail.com>
2020-07-14 21:16:45 -0500

    response question 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- create user only read one table
+-- user_0d_2 on your server (in localhost).
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
