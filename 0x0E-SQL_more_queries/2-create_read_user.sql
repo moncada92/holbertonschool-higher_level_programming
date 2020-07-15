2c92bbb3b176599097cc387f6ee9f77c722e3a48
Camilo Moncada <monk-da@hotmail.com>
2020-07-14 21:21:27 -0500

    hotfix question 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,4 @@
 -- user_0d_2 on your server (in localhost).
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT INSERT ON hbtn_0d_2.* TO bob@localhost;
