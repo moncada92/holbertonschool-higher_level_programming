370f94e18749e7cb28820dd4d0d1f93b879a8a37
Camilo Moncada <monk-da@hotmail.com>
2020-07-15 19:18:14 -0500

    response question 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows
+-- user_0d_2 on your server (in localhost).
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY title;
