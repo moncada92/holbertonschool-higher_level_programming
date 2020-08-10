d4826609fdb27f2b5e38d7e8d9314455d08d24ce
Camilo Moncada <monk-da@hotmail.com>
2020-08-10 00:00:52 -0500

    bugfix model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,4 +16,3 @@ class State(Base):
     id = Column(Integer, autoincrement=True, primary_key=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
