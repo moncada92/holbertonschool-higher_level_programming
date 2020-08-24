7242535f8168de904b82bc471f5a6e8cc2993713
Camilo Moncada <monk-da@hotmail.com>
2020-08-24 15:28:14 -0500

    hotfix 2 question 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,7 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    req = urllib.request.Request(sys.argv[1])
 
     with urllib.request.urlopen(req) as res:
         value = res.headers.get('X-Request-Id')
