b6b0a1f886c164bee19bcfd8b45b9b3bfc88f8be
Camilo Moncada <monk-da@hotmail.com>
2020-08-24 17:40:14 -0500

    response question 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""get request status code"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as res:
+            print(res.read())
+    except urllib.error.HTTPError as e:
+        print(e.code)
