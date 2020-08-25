5a360acb670f3b44ffca09355628c1876b592e56
Camilo Moncada <monk-da@hotmail.com>
2020-08-24 20:12:16 -0500

    response question 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" POST in API with requests """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    payload = {'q': ""}
+
+    if len(sys.argv) > 1:
+        payload['q'] = sys.argv[1]
+
+    r = requests.get("http://0.0.0.0:5000/search_user", data=payload)
+    if "json" not in r.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        if r.json():
+            res = r.json()
+            print("{} {}".format(res.get('id'), res.get('name')))
+        else:
+            print("No result")
