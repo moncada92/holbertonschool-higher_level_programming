9ad6317d5b996f79ea7675582ef8f00e39450e89
Camilo Moncada <monk-da@hotmail.com>
2020-08-24 20:21:30 -0500

    hotfix 3 question 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,16 +5,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-
-    payload = {'q': ""}
-    if len(sys.argv) > 1:
-        payload['q'] = sys.argv[1]
-    r = requests.get("http://0.0.0.0:5000/search_user", data=payload)
-    if "json" not in r.headers.get('content-type'):
+    json = {'q': ""}
+    if len(argv) > 1:
+        json['q'] = argv[1]
+    response = requests.post("http://0.0.0.0:5000/search_user", json)
+    if "json" not in response.headers.get('content-type'):
         print("Not a valid JSON")
     else:
-        if r.json():
-            print("[{}] {}".format(r.json().get('id'),
-                  r.json().get('name')))
+        if response.json():
+            print("[{}] {}".format(response.json().get('id'),
+                  response.json().get('name')))
         else:
             print("No result")
