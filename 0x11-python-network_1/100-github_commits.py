3d44433f6912e777b451a59c157a7dda3c46a7cf
Camilo Moncada <monk-da@hotmail.com>
2020-08-24 23:58:03 -0500

    response question 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" get id in github API """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    i = 0
+    repo_name = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(repo_name, owner)
+    r = requests.get(url)
+
+    if "json" not in r.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        res = r.json()
+        for data in res:
+            if i > 9:
+                break
+            print("{}: {}".format(data['sha'],
+                                  data['commit']['author']['name']))
+            i += 1
