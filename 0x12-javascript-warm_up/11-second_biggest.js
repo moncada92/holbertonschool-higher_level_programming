8f0709a3dd21f011165f1b1e7b4a2d44f96b8700
Camilo Moncada <monk-da@hotmail.com>
2020-09-15 17:16:47 -0500

    hotfix  question 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-const numbers = [];
-process.argv.forEach(n => {
-  numbers.push(n);
-});
-numbers.sort(function (a, b) {
-  return a - b;
-});
-console.log(numbers[numbers.length - 2]);
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const numbers = [];
+  process.argv.forEach(n => {
+    numbers.push(n);
+  });
+  numbers.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(numbers[numbers.length - 2]);
+}
