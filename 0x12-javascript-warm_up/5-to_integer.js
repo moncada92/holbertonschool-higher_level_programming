0ed3ca087774eb39bc1fc9e478964582bfe66bb2
Camilo Moncada <monk-da@hotmail.com>
2020-09-15 17:06:38 -0500

    hotfix 2 question 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) === false) {
-  console.log(`my numer: ${parseInt(process.argv[2])}`);
+  console.log(`my numer: ${parseInt(process.argv[2], 10)}`);
 } else {
   console.log('Not a number');
 }
