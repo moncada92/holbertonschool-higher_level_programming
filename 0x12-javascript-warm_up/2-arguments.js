8121eec7ef9b3c5cb5cd4ef723fa90d489c4bbe0
Camilo Moncada <monk-da@hotmail.com>
2020-09-15 16:48:31 -0500

    hotfix standard question 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const size = process.argv.length;
 if (size <= 2) {
-    console.log('No argument');
-}else {
+  console.log('No argument');
+} else {
   console.log('Argument found');
 }
