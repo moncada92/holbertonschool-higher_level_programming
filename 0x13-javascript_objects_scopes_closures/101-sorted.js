a414ec5db211dd82903bfe394381f51a86517929
Camilo Moncada <monk-da@hotmail.com>
2020-09-21 17:06:07 -0500

    response question 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const dict = require('./101-data').dict;
+
+const newdict = {
+  1: [],
+  2: [],
+  3: []
+};
+
+for (const key in dict) {
+  if (dict[key] === 1) {
+    newdict[1].push(key);
+  } else if (dict[key] === 2) {
+    newdict[2].push(key);
+  } else if (dict[key] === 3) {
+    newdict[3].push(key);
+  }
+}
+
+console.log(newdict);
