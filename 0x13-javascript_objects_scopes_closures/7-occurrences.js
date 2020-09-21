e39834dec69585307034df09bce9d67ff438da9e
Camilo Moncada <monk-da@hotmail.com>
2020-09-21 15:31:37 -0500

    hotfix question 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = (list, searchElement) => {
   let count = 0;
-  let founds = list.indexOf(searchElement);
-  while (founds !== -1) {
-    count++;
-    founds = list.indexOf(searchElement, founds + 1);
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
   }
-  console.log(count);
+  return count;
 };
