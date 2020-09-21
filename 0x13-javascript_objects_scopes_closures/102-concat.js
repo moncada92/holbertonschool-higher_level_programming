4a4097a3eec0dc784795e557d8ac84e322c6fec2
Camilo Moncada <monk-da@hotmail.com>
2020-09-21 18:23:32 -0500

    response question 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const fileA = fs.readFileSync(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log('error: ', err);
+  } else {
+    return data;
+  }
+});
+
+const fileB = fs.readFileSync(process.argv[3], 'utf-8', (err, data) => {
+  if (err) {
+    console.log('error: ', err);
+  } else {
+    return data;
+  }
+});
+
+const fileC = `${fileA}${fileB}`;
+
+fs.writeFile(process.argv[4], fileC, err => {
+  if (err) {
+    console.log('error write the file');
+  }
+});
