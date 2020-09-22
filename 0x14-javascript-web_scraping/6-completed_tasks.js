64a96047bc0a1fe55885f2a07a4b35dbc93160d0
Camilo Moncada <monk-da@hotmail.com>
2020-09-22 14:57:17 -0500

    response question 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const request = require('request');
+const url = `${process.argv[2]}`;
+let count = 0;
+
+request({
+  url: url,
+  json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  if (response.statusCode === 200) {
+    const store = {};
+    body.forEach(element => {
+      const { userId, completed } = element;
+      if (store[userId] === undefined) {
+        count = 0;
+        store[userId] = count;
+      }
+      if (completed) {
+        count++;
+        store[userId] = count;
+      }
+    });
+    console.log(store);
+  }
+});
