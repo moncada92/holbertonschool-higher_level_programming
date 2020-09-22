52d53123c8dc9ef0885520e5b0525d6ceb86486e
Camilo Moncada <monk-da@hotmail.com>
2020-09-22 15:38:35 -0500

    response question 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+
+request({
+  url: url,
+  json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    if (response.statusCode === 200) {
+      body.characters.forEach(element => {
+        request({
+          url: element,
+          json: true
+        }, (error, response, body) => {
+          if (error) {
+            console.log(error);
+          } else {
+            if (response.statusCode === 200) {
+              console.log(body.name);
+            }
+          }
+        });
+      });
+    }
+  }
+});
