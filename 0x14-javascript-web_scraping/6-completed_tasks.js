5b976149b6ddd9aeac4ea1518ff46fc5286f1b76
Camilo Moncada <monk-da@hotmail.com>
2020-09-22 15:10:00 -0500

    hotfix 3 question 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,19 +8,20 @@ request({
 }, (error, response, body) => {
   if (error) {
     console.log(error);
-  }
-  if (response.statusCode === 200) {
-    const store = {};
-    body.forEach(element => {
-      const { userId, completed } = element;
-      if (completed) {
-        if (store[userId]) {
-          store[userId]++;
-        } else {
-          store[userId] = 1;
+  } else {
+    if (response.statusCode === 200) {
+      const store = {};
+      body.forEach(element => {
+        const { userId, completed } = element;
+        if (completed) {
+          if (store[userId]) {
+            store[userId]++;
+          } else {
+            store[userId] = 1;
+          }
         }
-      }
-    });
-    console.log(store);
+      });
+      console.log(store);
+    }
   }
 });
